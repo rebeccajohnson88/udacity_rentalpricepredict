ca1f62be2c3771322c9f649f9391f9150cf2613f
rebeccajohnson88 <rebeccajohnson@Rebeccas-MacBook-Pro-2.local>
2022-07-21 14:52:29 -0400

    update git path

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ _steps = [
     "data_check",
     "data_split",
     "train_random_forest",
+    # We default to random forest but replace with train_gradient_boosting if trying that model
+    # "train_gradient_boosting",
     # NOTE: We do not include this in the steps so it is not run by mistake.
     # You first need to promote a model export to "prod" before you can run this,
     # then you need to run this step explicitly
